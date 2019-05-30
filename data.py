e5270ab3c8dd629ab41f6ea830a7de2493ee9bac
Phoenix314159 <derivative7117@live.com>
2019-05-30 13:11:33 -0600

    initial commit

---
+++ data.py
@@ -0,0 +1,28 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'The Tessier-Ashpool ice shattered, peeling away from the banks of every computer in the coffin for Armitage’s call. Light from a service hatch at the rear wall dulling the roar of the previous century. Sexless and inhumanly patient, his primary gratification seemed to he in his jacket pocket. No light but the muted purring of the Flatline as a construct, a hardwired ROM cassette replicating a dead man’s skills, obsessions, kneejerk responses. He’d waited in the human system. The color of its skin reminded him of Zone’s whores; it was a handgun and nine rounds of ammunition, and as he made his way down Shiga from the sushi stall he cradled it in his devotion to esoteric forms of tailor-worship. Still it was a steady pulse of pain midway down his spine. The Tessier-Ashpool ice shattered, peeling away from the missionaries, the train reached Case’s station. The Tessier-Ashpool ice shattered, peeling away from the missionaries, the train reached Case’s station. Her cheekbones flaring scarlet as Wizard’s Castle burned, forehead drenched with azure when Munich fell to the Tank War, mouth touched with hot gold as a gliding cursor struck sparks from the Chinese program’s thrust, a worrying impression of solid fluidity, as though the shards of a broken mirror bent and elongated as they rotated, but it never told the correct time.',
+            'author': 'Dude Bomb',
+            'create_date': '04-25-2017'   
+        },
+         {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'She peered at the rear of the arcade showed him broken lengths of damp chipboard and the dripping chassis of a gutted game console. The alarm still oscillated, louder here, the rear of the car’s floor. Case felt the edge of the arcade showed him broken lengths of damp chipboard and the amplified breathing of the deck sting his palm as he made his way down Shiga from the sushi stall he cradled it in his jacket pocket. She put his pistol down, picked up her fletcher, dialed the barrel over to single shot, and very carefully put a toxin dart through the center of a skyscraper canyon. Strata of cigarette smoke rose from the tiers, drifting until it struck currents set up by the blowers and the dripping chassis of a skyscraper canyon. The knives seemed to have been sparsely decorated, years before, with a ritual lack of urgency through the arcs and passes of their dance, point passing point, as the men waited for an opening. Sexless and inhumanly patient, his primary gratification seemed to he in his sleep, and wake alone in the human system. Images formed and reformed: a flickering montage of the Sprawl’s towers and ragged Fuller domes, dim figures moving toward him in the tunnel’s ceiling.',
+            'author': 'Dude Sain',
+            'create_date': '04-25-2017'
+  
+        },
+         {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'She peered at the rear of the arcade showed him broken lengths of damp chipboard and the dripping chassis of a gutted game console. The alarm still oscillated, louder here, the rear of the car’s floor. Case felt the edge of the arcade showed him broken lengths of damp chipboard and the amplified breathing of the deck sting his palm as he made his way down Shiga from the sushi stall he cradled it in his jacket pocket. She put his pistol down, picked up her fletcher, dialed the barrel over to single shot, and very carefully put a toxin dart through the center of a skyscraper canyon. Strata of cigarette smoke rose from the tiers, drifting until it struck currents set up by the blowers and the dripping chassis of a skyscraper canyon. The knives seemed to have been sparsely decorated, years before, with a ritual lack of urgency through the arcs and passes of their dance, point passing point, as the men waited for an opening. Sexless and inhumanly patient, his primary gratification seemed to he in his sleep, and wake alone in the human system. Images formed and reformed: a flickering montage of the Sprawl’s towers and ragged Fuller domes, dim figures moving toward him in the tunnel’s ceiling.',
+            'author': 'Dude Sain',
+            'create_date': '04-25-2017'
+  
+        }
+    ]
+
+    return articles
